d4f98f67ff5d49e74cca5dc03b6300f8fb01df8a
Bryan Estrito <bryan.estrito@gmail.com>
2017-02-20 10:31:26 +0800

    VideoConverter library with ffmpeg uncomplete

--- app/Libraries/VideoConverter.php
+++ app/Libraries/VideoConverter.php
@@ -2,12 +2,10 @@
 
 class VideoConverter
 {
+    protected $mediaFile;
     protected $filename;
 
-    // id v3 tags
-    protected $title;
-    protected $artist;
-    protected $album;
+    protected $tags;
 
     // cutting
     protected $startTime;
@@ -16,14 +14,39 @@ class VideoConverter
     const DOWNLOAD_FOLDER = '/downloads/';
     const CONVERTER_FOLDER = '/converted/';
 
-    public function __construct($mediaFile, $filename)
+    public function __construct($mediaFile, $filename = null, $tags = null)
     {
         $this->mediaFile = $mediaFile;
+
+        $this->setFilename($filename);
+        $this->setID3v2Tags($tags);
+    }
+
+    public function setFilename($filename)
+    {
+        if (!$filename) {
+            $filename = 'something';
+        }
+
         $this->filename = $filename;
     }
 
     public function setID3v2Tags($tags)
     {
-        # code...
+        $ffmpegTags = ['title', 'artist', 'album'];
+
+        foreach ($tags as $t => $tag) {
+            if (!in_array($tag, $ffmpegTags)) {
+                throw new Exception("Invalid meta tag!");
+            }
+        }
+
+        $this->tags = $tags;
+    }
+
+    public function cutStartEndTime($startTime = null, $endTime = null)
+    {
+        $this->startTime = $startTime;
+        $this->endTime = $endTime;
     }
 }
